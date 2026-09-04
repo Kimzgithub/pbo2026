3cdaadf0544e3a7e11748ec00ba1acac95d61ae8
Kimzgithub <kimzgithub@gmail.com>
2026-09-04 15:22:53 +0700

    Praktikum 3C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -1,10 +1,14 @@
 <?php
 
-$lingkarz = new Lingkar();
-$lingkarz ->jarijari = 7;
+$lingkarz = new Lingkar(7);
+$lingkarz ->jarijari;
 class Lingkar {
     const PHI = 3.14;
     public float $jarijari;
+
+    public function __construct(float $jarijari) {
+        $this->jarijari = $jarijari;
+    }
 
     public function luas() : float {
         return self::PHI*$this->jarijari*$this->jarijari;
@@ -19,12 +23,15 @@ echo "luas lingkaran : ". $lingkarz->luas(). "cm2\n";
 echo "keliling lingkaran : ". $lingkarz->keliling(). "cm2\n";
 
 
-$nasirempah =  new Setlingkaran();
-$nasirempah -> jarijari = 4;
+$nasirempah =  new Setlingkaran(4);
+$nasirempah -> jarijari;
 class Setlingkaran {
     const PHI = 3.14;
     public float $jarijari;
 
+    public function __construct(float $jarijari) {
+        $this->jarijari = $jarijari;
+    }
     public function luas() : float {
         return (1/2)*self::PHI*$this->jarijari*$this->jarijari;
     }
@@ -37,11 +44,15 @@ class Setlingkaran {
 echo "luas Nasi Rempah dengan setengah lingkaran : ". $nasirempah->luas(). "cm2\n";
 echo "keliling Nasi Rempah dengan setengah lingkaran : ". $nasirempah->keliling(). "cm\n";
 
-$bola = new Bola();
-$bola->jarijari = 10;
+$bola = new Bola(10);
+$bola->jarijari;
 class Bola {
     const PHI = 3.14;
     public float $jarijari;
+
+    public function __construct(float $jarijari) {
+        $this->jarijari = $jarijari;
+    }
 
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jarijari,3);
@@ -50,25 +61,37 @@ class Bola {
 
 echo "Volume Bola : ". $bola->volume() ."cm3\n";
 
-$tabung = new Tabung();
-$tabung->jarijari = 7;
-$tabung->tinggi = 10;
+$tabung = new Tabung(7, 10);
+$tabung->jarijari;
+$tabung->tinggi;
 class Tabung {
     const PHI = 3.14;
     public float $jarijari;
     public float $tinggi;
+
+    public function __construct(float $jarijari, float $tinggi) {
+        $this->jarijari = $jarijari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return self::PHI*pow($this->jarijari,2) * $this->tinggi;
     }
 }
 
-$kerucut = new Kerucut();
-$kerucut->jarijari = 7;
-$kerucut->tinggi = 10;
+$kerucut = new Kerucut(7, 10);
+$kerucut->jarijari;
+$kerucut->tinggi;
 class Kerucut{
     const PHI = 3.14;
     public float $jarijari;
     public float $tinggi;
+
+    public function __construct(float $jarijari, float $tinggi) {
+        $this->jarijari = $jarijari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jarijari,2)*$this->tinggi;
     }
