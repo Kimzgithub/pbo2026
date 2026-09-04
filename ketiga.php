b60e86d88f470e7e10541da800de3818263022d9
Kimzgithub <kimzgithub@gmail.com>
2026-09-04 14:25:40 +0700

    Praktikum 3A - Membuat Class

--- ketiga.php
+++ ketiga.php
@@ -21,6 +21,7 @@ class Setlingkaran {
     public function luas($jarijari) : float {
         return (1/2)*self::PHI*$jarijari*$jarijari;
     }
+    
     public function keliling($jarijari) : float {
         return self::PHI*$jarijari;
     }
