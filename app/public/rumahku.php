0381a7195b30f8edcf5acbde3c17d32c94b8d0c5
Kimzgithub <kimzgithub@gmail.com>
2026-09-18 14:16:50 +0700

    salah ulang commit

--- app/public/rumahku.php
+++ app/public/rumahku.php
@@ -1,8 +1,8 @@
 <?php
 
 
-require_once '../app/Site/HomePage.php';
-require_once '../app/Site/Admin/HomePage.php';
+require_once 'app/Site/HomePage.php';
+require_once 'app/Site/Admin/HomePage.php';
 
 
 $hp = new HomePage();
